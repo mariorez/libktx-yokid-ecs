e6d1a5289afef4a111796f14bfef61f4b6f9e189
Mario Rezende <mariorez@gmail.com>
2021-12-29 17:01:42 -0300

    refactor: add more frames to idle animation

--- core/src/main/kotlin/com/mariorez/component/AnimationComponent.kt
+++ core/src/main/kotlin/com/mariorez/component/AnimationComponent.kt
@@ -3,18 +3,20 @@ package com.mariorez.component
 import com.badlogic.ashley.core.Component
 import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode.LOOP
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode.NORMAL
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.Pool.Poolable
 import ktx.ashley.mapperFor
 
 enum class AnimationType(
     val key: String,
-    val playMode: PlayMode = PlayMode.NORMAL,
+    val playMode: PlayMode = NORMAL,
     val frames: Int = 1,
     val frameDuration: Float = 1f
 ) {
-    IDLE("idle"),
-    RUNNING("run", PlayMode.LOOP, 8, 0.07f)
+    IDLE("idle", LOOP, 7, 0.1f),
+    RUNNING("run", LOOP, 8, 0.07f)
 }
 
 class AnimationComponent : Component, Poolable {
